f1bac9a84499c46a994f02d64c74282abca362a4
mariovarela99 <mariovarela99@outlook.com>
2022-05-01 22:08:46 +0100

    Lighting

--- src/AppClass.js
+++ src/AppClass.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class App extends Component {
   state = {
     count: 0,
+    isOn: false,
   };
 
   increment = () => {
@@ -11,11 +12,29 @@ class App extends Component {
     }));
   };
 
+  toggleLight = () => {
+    this.setState((prevState) => ({
+      isOn: !prevState.isOn,
+    }));
+  };
+
   render() {
     return (
-      <button onClick={this.increment}>
-        I was clicked {this.state.count} times
-      </button>
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.increment}>
+          I was clicked {this.state.count} times
+        </button>
+        <h2> Toggle Light </h2>
+        <div
+          style={{
+            height: "50px",
+            width: "50px",
+            background: this.state.isOn ? "yellow" : "grey",
+          }}
+          onClick={this.toggleLight}
+        />
+      </>
     );
   }
 }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,11 +2,36 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const incrementCount = () => {
     setCount((prevCount) => prevCount + 1);
   };
-  return <button onClick={incrementCount}>I was clicked {count} times</button>;
+
+  const toggleLight = () => {
+    setIsOn((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>I was clicked {count} times</button>
+      <h2> Toggle Light </h2>
+      <img
+        src={
+          isOn
+            ? "https://icon-library.com/images/highlights-icon/highlights-icon-27.jpg"
+            : "https://icon-library.com/images/9d33aedf9c_62030.png"
+        }
+        style={{
+          height: "150px",
+          width: "150px",
+        }}
+        alt="FlashLight"
+        onClick={toggleLight}
+      />
+    </>
+  );
 };
 
 export default App;
