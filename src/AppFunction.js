cbb6539f60ad7a5437296f43ab91dae1fa8479be
mariovarela99 <mariovarela99@outlook.com>
2022-05-07 21:19:50 +0100

    Set STatus off and online with useEffect

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -4,6 +4,7 @@ const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine);
 
   const incrementCount = () => {
     setCount((prevCount) => prevCount + 1);
@@ -12,11 +13,23 @@ const App = () => {
   useEffect(() => {
     document.title = `You have clicked ${count} times`;
     window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
     };
   }, [count]);
+
+  const handleOnline = () => {
+    setStatus(true);
+  };
+
+  const handleOffline = () => {
+    setStatus(false);
+  };
 
   const handleMouseMove = (event) => {
     setMousePosition({
@@ -50,6 +63,12 @@ const App = () => {
 
       <h2>Mouse Position</h2>
       {JSON.stringify(mousePosition, null, 2)}
+
+      <h2>NetWork Status: </h2>
+      <p>
+        {" "}
+        You are <strong>{status ? "Online" : "Offline"}</strong>{" "}
+      </p>
     </>
   );
 };
